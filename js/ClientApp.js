d701cd07eec48e4c4b37d70af6b3062a78effa9d
Pooria <pooria@Poorias-MacBook-Pro.local>
2017-02-21 20:18:29 -0700

    made 2 simple components

---
+++ js/ClientApp.js
@@ -0,0 +1,28 @@
+var div = React.DOM.div
+var h1 = React.DOM.h1
+
+var MyTitle = React.createClass({
+  render: function () {
+    return (
+      div(null,
+        h1(null,this.props.anyTitle)
+      )
+    )
+  }
+})
+
+var MyTitleFactory = React.createFactory(MyTitle)
+
+var MyFirstComponent = React.createClass({
+  render: function() {
+    return (
+      div(null,
+        MyTitleFactory({anyTitle: 'A'}),
+        MyTitleFactory({anyTitle: 'B'}),
+        React.createElement(MyTitle, {anyTitle: 'C'})
+      )
+    )
+  }
+})
+
+ReactDOM.render(React.createElement(MyFirstComponent), document.getElementById('app'))
