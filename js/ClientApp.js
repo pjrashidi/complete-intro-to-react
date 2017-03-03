7ba4cd139847cb23c1d80bd3ca4bf3e9fc284237
Pooria <pjrashidi@gmail.com>
2017-03-02 20:55:46 -0600

    use webpack to break the MyTitle component off from clientApp

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -1,18 +1,5 @@
 /* global React ReactDOM */
 
-var div = React.DOM.div
-var h1 = React.DOM.h1
-
-var MyTitle = React.createClass({
-  render: function () {
-    return (
-      div(null,
-        h1({ style: { color: this.props.color, fontWeight: '100' } }, this.props.anyTitle)
-      )
-    )
-  }
-})
-
 var MyTitleFactory = React.createFactory(MyTitle)
 
 var MyFirstComponent = React.createClass({
