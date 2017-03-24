58f0ed4bce300652beb0e0547c897da5ec8df991
Pooria <pjrashidi@gmail.com>
2017-03-23 19:38:08 -0700

    added show-card markup

--- js/Search.js
+++ js/Search.js
@@ -7,7 +7,14 @@ const Search = React.createClass({
       <div className='search'>
         {preload.shows.map((show) => {
           return (
-            <h3>{show.title}</h3>
+            <div className='show-card'>
+              <img src={`/public/img/posters/${show.poster}`} />
+              <div>
+                <h3>{show.title}</h3>
+                <h4>({show.year})</h4>
+                <p>{show.description}</p>
+              </div>
+            </div>
           )
         })}
       </div>
