492a2b0a19d1a0d14dcb2afff7af50668ee57467
Pooria <pjrashidi@gmail.com>
2017-03-25 17:45:32 -0700

    added state to search.js and made the search filter out showcards

--- js/Search.js
+++ js/Search.js
@@ -3,14 +3,35 @@ import preload from '../public/data.json'
 import ShowCard from './ShowCard.js'
 
 const Search = React.createClass({
+  getInitialState () {
+    return {
+      searchTerm: ''
+    }
+  },
+  handleSearchTermChange (event) {
+    this.setState({searchTerm: event.target.value})
+  },
   render () {
     return (
       <div className='search'>
-        {preload.shows.map((show) => {
-          return (
-            <ShowCard key={show.imdbID} {...show} />
-          )
-        })}
+        <header>
+          <h1>Vidja</h1>
+          <input
+            onChange={this.handleSearchTermChange}
+            value={this.state.searchTerm}
+            type='text' placeholder='Search' />
+        </header>
+        <div>
+          {preload.shows
+          .filter((show) => {
+            return `${show.title} ${show.description}`.toUpperCase().indexOf(this.state.searchTerm.toUpperCase()) >= 0
+          })
+          .map((show) => {
+            return (
+              <ShowCard key={show.imdbID} {...show} />
+            )
+          })}
+        </div>
       </div>
     )
   }
