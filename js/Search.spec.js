cc27719a295517c566697a6bf82f4f256ecd206f
Pooria <pjrashidi@gmail.com>
2017-04-14 22:24:09 -0700

    updated snapshot and refactored tests

--- js/Search.spec.js
+++ js/Search.spec.js
@@ -1,25 +1,29 @@
 import React from 'react'
-import { shallow } from 'enzyme'
+import { shallow, render } from 'enzyme'
 import { shallowToJson } from 'enzyme-to-json'
+import { Provider } from 'react-redux'
+import store from './store'
+import { setSearchTerm } from './actionCreators'
+import { Unwrapped as UnwrappedSearch } from './Search.js'
 import Search from './Search'
 import ShowCard from './ShowCard'
 import preload from '../public/data.json'
 
 test('Search snapshot test', () => {
-  const component = shallow(<Search />)
+  const component = shallow(<UnwrappedSearch shows={preload.shows} searchTerm='' />)
   const tree = shallowToJson(component)
   expect(tree).toMatchSnapshot()
 })
 
 test('Search should render a ShowCard for each show', () => {
-  const component = shallow(<Search />)
+  const component = shallow(<UnwrappedSearch shows={preload.shows} searchTerm='' />)
   expect(component.find(ShowCard).length).toEqual(preload.shows.length)
 })
 
 test('Search should render correct number of shows based on search', () => {
   const searchWord = "house"
-  const component = shallow(<Search />)
+  store.dispatch(setSearchTerm(searchWord))
+  const component = render(<Provider store={store}><Search shows={preload.shows} /></Provider>)
   const ShowCount = preload.shows.filter((show) => `${show.title} ${show.description}`.toUpperCase().indexOf(searchWord.toUpperCase()) >= 0).length
-  component.find('input').simulate('change', {target: {value: searchWord}})
-  expect(component.find(ShowCard).length).toEqual(ShowCount)
+  expect(component.find('.show-card').length).toEqual(ShowCount)
 })
